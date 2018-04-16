a6a1180d8795f94621131728264383c286bd95e3
Anuar Otynshin <strigeandro@gmail.com>
2018-04-17 00:27:11 +0600

    Update Description Activity design

--- app/src/main/java/com/example/strig/baiterekapp/DescriptionActivity.java
+++ app/src/main/java/com/example/strig/baiterekapp/DescriptionActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.strig.baiterekapp.fragments.GalleryFragment;
+
 public class DescriptionActivity extends AppCompatActivity {
 
     GalleryFragment gallery;
---
+++ app/src/main/java/com/example/strig/baiterekapp/fragments/DescriptionFragment.java
@@ -0,0 +1,26 @@
+package com.example.strig.baiterekapp.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.strig.baiterekapp.R;
+
+public class DescriptionFragment extends Fragment {
+
+
+    public DescriptionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_description, container, false);
+    }
+
+}
