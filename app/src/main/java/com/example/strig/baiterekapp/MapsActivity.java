df67a5e0fbb9bfa35b5b7de20651a31387f915d3
Anuar Otynshin <strigeandro@gmail.com>
2018-04-16 22:43:06 +0600

    Add Firebase Cloud Messaging

--- app/src/main/java/com/example/strig/baiterekapp/MapsActivity.java
+++ app/src/main/java/com/example/strig/baiterekapp/MapsActivity.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -101,9 +101,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         });
     }
-
-
-
 
     /**
      * Manipulates the map once available.
---
+++ app/src/main/java/com/example/strig/baiterekapp/MessagingFBService.java
@@ -0,0 +1,17 @@
+package com.example.strig.baiterekapp;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MessagingFBService extends FirebaseInstanceIdService {
+    public final String TAG = "FIREBASE_TAG";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+    }
+}
