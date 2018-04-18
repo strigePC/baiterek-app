845daece1e31db4cd3d08aa88c89149e25a32f13
Anuar Otynshin <strigeandro@gmail.com>
2018-04-19 01:15:12 +0600

    Change Table

--- app/src/main/java/com/example/strig/baiterekapp/LeaderboardActivity.java
+++ app/src/main/java/com/example/strig/baiterekapp/LeaderboardActivity.java
@@ -2,27 +2,25 @@ package com.example.strig.baiterekapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.Map;
+
 public class LeaderboardActivity extends AppCompatActivity {
-    LinearLayout leaderboardLayout;
+    TextView entries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
 
-        leaderboardLayout = findViewById(R.id.leaderboard_layout);
+        entries = findViewById(R.id.leadeboard_entries);
+    }
 
-        TextView entry = new TextView(this);
-        entry.setLayoutParams(new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-
-        entry.setText(String.format(getResources().getString(R.string.leaderboard_entry_text),
-                "Anuar", "5/5"));
-
-        leaderboardLayout.addView(entry);
+    public void populateLeaderboard(Map<String, Long> map) {
+        for (Map.Entry<String, Long> entry : map.entrySet()) {
+            entries.setText(entries.getText().toString() + "\n"
+                    + String.format(getResources().getString(R.string.leaderboard_entry_text), entry.getKey(), String.valueOf(entry.getValue())));
+        }
     }
 }
