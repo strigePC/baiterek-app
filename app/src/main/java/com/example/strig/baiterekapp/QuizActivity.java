3bc255124e7edb1826e6afe5b4049f59ae35e8e3
Anuar Otynshin <strigeandro@gmail.com>
2018-04-19 00:02:37 +0600

    Add EditText to Quiz

--- app/src/main/java/com/example/strig/baiterekapp/QuizActivity.java
+++ app/src/main/java/com/example/strig/baiterekapp/QuizActivity.java
@@ -6,8 +6,9 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,14 +22,14 @@ public class QuizActivity extends AppCompatActivity {
     private List<Question> questions;
     private List<String> correctAnswers;
 
-    LinearLayout quizLayout;
-
     TextView questionTitle;
     TextView question;
     List<Button> answerButtons;
     Button answerButton1;
     Button answerButton2;
     Button answerButton3;
+
+    EditText username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,10 +51,9 @@ public class QuizActivity extends AppCompatActivity {
         questions.add(new Question(getResources().getString(R.string.question_4), answers4));
         questions.add(new Question(getResources().getString(R.string.question_5), answers5));
 
-        quizLayout = findViewById(R.id.quiz_layout);
-
         questionTitle = findViewById(R.id.question_title);
         question = findViewById(R.id.question);
+        username = findViewById(R.id.username);
         answerButtons = new ArrayList<>();
         answerButton1 = findViewById(R.id.answer_1);
         answerButton2 = findViewById(R.id.answer_2);
@@ -107,16 +107,20 @@ public class QuizActivity extends AppCompatActivity {
                                 questionTitle.setText(getResources().getString(R.string.quiz_completed));
                                 question.setText(String.format(getResources().getString(R.string.score_text), userScore, questions.size()));
                                 for (Button answerButton : answerButtons) {
-                                    answerButton.setVisibility(View.INVISIBLE);
+                                    answerButton.setVisibility(View.GONE);
                                 }
+                                username.setVisibility(View.VISIBLE);
                                 answerButtons.get(1).setVisibility(View.VISIBLE);
                                 answerButtons.get(1).setText(R.string.back_to_menu);
                                 answerButtons.get(1).setBackground(getResources().getDrawable(R.drawable.button_green));
                                 answerButtons.get(1).setOnClickListener(new View.OnClickListener() {
                                     @Override
                                     public void onClick(View view) {
-                                        Intent i = new Intent(QuizActivity.this, DescriptionActivity.class);
-                                        startActivity(i);
+                                        if (!username.getText().toString().equals("")) {
+                                            Intent i = new Intent(QuizActivity.this, DescriptionActivity.class);
+                                            startActivity(i);
+                                            Toast.makeText(QuizActivity.this, "Your result was saved", Toast.LENGTH_SHORT).show();
+                                        }
                                     }
                                 });
                             }
