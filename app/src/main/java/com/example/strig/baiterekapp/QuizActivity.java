1ec0c17d4c5fd3e1c4ca13f939f1bb78ff64e1f8
Anuar Otynshin <strigeandro@gmail.com>
2018-04-21 13:03:50 +0600

    Make Quiz not accept spaces

--- app/src/main/java/com/example/strig/baiterekapp/QuizActivity.java
+++ app/src/main/java/com/example/strig/baiterekapp/QuizActivity.java
@@ -127,7 +127,7 @@ public class QuizActivity extends AppCompatActivity {
                                     @Override
                                     public void onClick(View view) {
                                         username = usernameEditText.getText().toString();
-                                        if (!username.equals("")) {
+                                        if (!username.isEmpty()) {
                                             FirebaseDatabase database = FirebaseDatabase.getInstance();
                                             DatabaseReference myRef = database.getReference("Quiz/"+username);
                                             myRef.setValue(userScore);
